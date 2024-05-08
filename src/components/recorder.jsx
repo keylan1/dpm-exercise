6eee7aea62c2616ecbc02b178038a32ba5d9bd2f
Sarah.Keller <sarah29.84@gmail.com>
2024-05-08 18:23:38 +0200

    Fixed recording

--- src/components/recorder.jsx
+++ src/components/recorder.jsx
@@ -3,7 +3,8 @@ import Button from '@mui/material/Button';
 
 //Usage of ReactMediaRecorder version 1.4.0 (later v cause broker-factory error)
 
-//
+//Technically, permission should be asked to access the controls etc.
+//firebase with oauth2 would simplify storage and authentication
 const Recorder = () => (
   <div>
     <ReactMediaRecorder
@@ -11,6 +12,7 @@ const Recorder = () => (
       render={({
         status,
         pauseRecording,
+        resumeRecording,
         startRecording,
         stopRecording,
         mediaBlobUrl,
@@ -19,6 +21,7 @@ const Recorder = () => (
           <p>{status}</p>
           <Button onClick={startRecording}>Start Recording</Button>
           <Button onClick={pauseRecording}>Pause Recording</Button>
+          <Button onClick={resumeRecording}>Resume Recording</Button>
           <Button onClick={stopRecording}>Stop Recording</Button>
           <audio src={mediaBlobUrl} controls />
         </div>
